60a81f895017ff613124279210984ec2dc47c59b
jlc25 <jinglingchen25@gmail.com>
2026-06-01 10:34:34 -0400

    Merged main into branch

--- frontend/src/app/(auth)/user-signup-form/page.tsx
+++ frontend/src/app/(auth)/user-signup-form/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { redirect } from "next/navigation";
+import { signup } from "@/src/lib/api";
 
 interface technicalSkill {
   tskill: string;
